06d6b94facd894988bfd56be1eaa4bf3a8dc441a
stogonoff12 <128945141+strogonoff12@users.noreply.github.com>
2024-03-17 20:52:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function somar(a, b){
+    console.log(a+b)
+}
+
+function subtrair(a, b){
+    console.log(a-b)
+}
+
+function dividir(a, b){
+    console.log(a/b)
+}
+
+function multiplicar(a, b){
+    console.log(a*b)
+}
